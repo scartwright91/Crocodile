656bab806c21f9d5f5c07fe672e72be13cb2c44d
Samuel Cartwright <sam.cartwright123@hotmail.co.uk>
2023-12-16 17:32:04 +0100

    Added 3d Renderer class

---
+++ Engine/src/Crocodile/s3d/rendering/Renderer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../../Core.h"
+
+namespace Crocodile
+{
+    namespace s3d
+    {
+        class CROCODILE_API Renderer
+        {
+            public:
+                Renderer();
+                ~Renderer();
+
+            private:
+                void init();
+
+        };
+    }
+}
--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -11,9 +11,6 @@ class Sandbox : public Crocodile::Application
 
 public:
 
-    s2d::Object* obj = new s2d::Object();
-    s2d::Object* obj2 = new s2d::Object();
-
     Sandbox() : Crocodile::Application("Sandbox", false, 1280, 720, false)
     {
         init();
@@ -21,7 +18,7 @@ public:
 
     ~Sandbox()
     {
-        delete obj;
+
     }
 
     void update(float dt)
@@ -31,21 +28,7 @@ public:
 
     void init()
     {
-        s2d::Layer* layer = new s2d::Layer("objects");
-        scene2d->layerStack->addLayer(layer);
 
-        obj->setPosition(glm::vec2(400.f));
-        obj->size = glm::vec2(50.f);
-        obj->color = glm::vec3(1.f, 0.f, 0.f);
-        
-        obj2->setPosition(glm::vec2(50.f));
-        obj2->size = glm::vec2(10.f);
-        obj2->color = glm::vec3(1.f, 0.f, 1.f);
-        
-        obj->addChildObject(obj2);
-
-        scene2d->addObject(obj, "objects");
-        scene2d->camera->setTarget(obj, false);
     }
 
 };
